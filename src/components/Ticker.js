84c44908d60de00ce145da6650690e39968b11fb
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-09-29 14:54:42 +0300

    Done

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,11 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { makeRandomNumber } from "../utils";
 
 function Ticker() {
   const [price, setPrice] = useState(0);
   const [color, setColor] = useState("black");
+  //create the ref and set its initial value
+  const prevPriceRef = useRef(price)
 
   useEffect(() => {
+    const prevPrice = prevPriceRef.current
+    if (price > prevPrice){
+      setColor("green");
+    } else if (price < prevPrice){
+      setColor("red")
+    } else {
+      setColor("black")
+    }
+ //set the new value of the ref (this does not trigger a re-render)
+    prevPriceRef.current = price;
+  },[price])
+
+  useEffect(() => {
+    //every 1 second, generate a new random price
     const id = setInterval(() => setPrice(makeRandomNumber), 1000);
     return function () {
       clearInterval(id);
